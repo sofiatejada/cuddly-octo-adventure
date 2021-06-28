b1fd2f74f9a62b107b64ad438fd38ae48a1516a2
Sofia Tejada <sofianais@icloud.com>
2021-06-27 21:31:43 -0700

    get weather function ready and passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 
@@ -10,11 +11,11 @@ describe('app routes', () => {
     test('returns city data', async() => {
 
       const expectation =
-        {
-          'formatted_query': 'Seattle, WA, USA',
-          'latitude': '47.606210',
-          'longitude': '-122.332071'
-        };
+      {
+        'formatted_query': 'Seattle, King County, Washington, USA',
+        'latitude': '47.6038321',
+        'longitude': '-122.3300624',
+      };
 
       const data = await fakeRequest(app)
         .get('/location?search=seattle')
@@ -23,5 +24,35 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expectation);
     });
+
+    test('returns weater data', async() => {
+
+      const expectation = [
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/weather?latitude=37.130408&longitude=-121.6544974')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
   });
 });
